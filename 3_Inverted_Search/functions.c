6322be21f84880bec4f95d9a11b4a742ce68592c
Sharan Rathnakumar <53375728+sharanrathnakumar@users.noreply.github.com>
2023-02-17 21:52:04 +0530

    Update functions.c

--- 3_Inverted_Search/functions.c
+++ 3_Inverted_Search/functions.c
@@ -1,32 +1,10 @@
 #include "inverted_index.h"
 
-void validate_n_store_filenames(file_node_t **head, char *filenames[])
-{
-    for (int i = 1; filenames[i] != NULL; i++)
-    {
-        int ret = IsFileValid(filenames[i]);
-        if (ret == SUCCESS) {
-            ret = store_filenames_to_list(filenames[i], head);
-            if(ret == REPEATED)
-            printf("Error: Filename is repeated\n");
-            else
-            {
-                // CREATE DB /////////////////////////////////////////////////////////////
-            }
-        }
-        else if(ret == NOT_PRESENT)
-        printf("Error: File not Found\n");
-        else
-        printf("Error: Empty File\n");
-
-    }
-}
-
 int IsFileValid(char *filename)
 {
     FILE *fptr;
     long size;
-   // printf("%s\n", filename);
+    // printf("%s\n", filename);
     fptr = fopen(filename, "r");
     if (fptr == NULL)
         return NOT_PRESENT;
@@ -39,24 +17,77 @@ int IsFileValid(char *filename)
         return SUCCESS;
 }
 
-int store_filenames_to_list(char *filename, file_node_t **head)
+int store_filenames_to_list(char *filename, file_node_t **file_f_head)
 {
     file_node_t *new = malloc(sizeof(file_node_t));
-    strcpy(new->f_name,filename);
+    strcpy(new->f_name, filename);
     new->link = NULL;
-    if (*head == NULL)
+    if (*file_f_head == NULL)
     {
-        *head = new;
+        *file_f_head = new;
         return SUCCESS;
     }
-    file_node_t *ptr = *head,*prev;
-    while(ptr != NULL)
+    file_node_t *ptr = *file_f_head, *prev;
+    while (ptr != NULL)
     {
         prev = ptr;
-        if(!strcmp(ptr->f_name,new->f_name))
-        return REPEATED;
+        if (!strcmp(ptr->f_name, new->f_name))
+            return REPEATED;
         ptr = ptr->link;
     }
     prev->link = new;
     return SUCCESS;
+}
+
+int create_DB(file_node_t *file_f_head, main_node_t **f_head)
+{
+
+    while (file_f_head != NULL)
+    {
+        read_datafile(file_f_head, f_head, file_f_head->f_name);
+        file_f_head = file_f_head->link;
+    }
+}
+
+void read_datafile(file_node_t *file_f_head, main_node_t **f_head, char *f_name)
+{
+    FILE *fptr = fopen(f_name, "r");
+    main_node_t *ptr[26] = {NULL};
+    int index = toupper(f_name[0]) % 65;
+    *f_head = ptr[index];
+    char buff[NAMELENGTH];
+    while(!feof(fptr)) {
+    fscanf(fptr, "%s", buff);
+    insert_at_last_main(f_head, buff);
+    }
+}
+
+int insert_at_last_main(main_node_t **head, char *word)
+{
+    int index = toupper(word[0]) % 65;
+    printf("Index: %d\n",index);
+    
+    main_node_t *new = malloc(sizeof(main_node_t));
+
+    if(head[index]->link == NULL)
+    {
+        printf("Inner Loop\n");
+        head[index]->link = new;
+        new->f_count = 1;
+        new->link = NULL;
+        strcpy(new->word,word);
+        printf("Word: %s\n", new->word);
+        new->sub_link = add_sub_node(word);
+        return SUCCESS;
+    }
+     
+}
+
+sub_node_t * add_sub_node(char * word)
+{
+    sub_node_t *newSub = malloc(sizeof(sub_node_t));
+    strcpy(newSub->f_name,word);
+    newSub->link = NULL;
+    newSub->w_count = 1;
+    return newSub;
 }
